59e96bdeca8855338ea42268d9b6f148220171c6
streq <strequena@gmail.com>
2022-08-12 14:36:41 -0300

    no se que agregue, resistencia cuadrada en lugar de lineal

--- src/flapper/flapper.gd
+++ src/flapper/flapper.gd
@@ -2,6 +2,7 @@ extends KinematicBody2D
 
 
 export var gravity = 200.0
+export var air_friction = 0.0
 
 
 var velocity = Vector2()
@@ -25,6 +26,11 @@ func _physics_process(delta):
 #	print("point_dir:",point_dir," stepify:",stepify(point_dir,PI/8.0))
 	state_machine.physics_update(delta)
 	velocity += Vector2.DOWN*gravity*delta
-#	velocity *= 0.99
 	
+	#air_friction = velocity*velocity*k
+	var air_friction_force = velocity.length()*air_friction
+	velocity *= (1-air_friction_force*delta)
 	velocity = move_and_slide(velocity)
+	if velocity:
+		print(velocity)
+	
--- src/flapper/glide.gd
+++ src/flapper/glide.gd
@@ -11,16 +11,18 @@ func _physics_update(delta):
 	var vel :Vector2 = owner.velocity
 	var normal :Vector2 = owner.direction
 	var projected :Vector2 = vel.project(normal)
-	
+
 	if normal.dot(vel) < 0.0:
-#		zprint("vel:",vel,", normal:",normal,", projected:",projected)
+#		print("vel:",vel,", normal:",normal,", projected:",projected)
 		
 		if projected.length()>150.0:
 			owner.state_animation.play("glide_fail")
-			owner.velocity -= projected*2.5*delta
+			owner.velocity -= projected*projected.length()*0.01*delta
 		else:
 			owner.state_animation.play("glide")
-			owner.velocity -= projected*5.0*delta
+			owner.velocity -= projected*projected.length()*0.1*delta
+			
+		
 	elif projected.length()>250.0:
 		owner.state_animation.play("glide_opposite_fail")
 		
