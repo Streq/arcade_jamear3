ea64976a39a31174c7a1b18d5ee3a7c9e515fb14
streq <strequena@gmail.com>
2022-09-23 14:36:37 -0300

    intro mas piola

--- src/gamemode/single_room/title.gd
+++ src/gamemode/single_room/title.gd
@@ -1,8 +1,3 @@
 extends Control
 
 
-func _ready() -> void:
-	get_tree().paused = true
-	yield(get_tree().create_timer(3.0,true),"timeout")
-	get_tree().paused = false
-	queue_free()
--- src/intro/intro_game.gd
+++ src/intro/intro_game.gd
@@ -3,14 +3,17 @@ extends CanvasLayer
 var app_state_node
 
 
-
-func _on_AnimationPlayer_animation_finished(anim_name):
-	exit()
 func _input(event):
 	if event.is_action_pressed("A") or event.is_action_pressed("B"):
 		exit()
 
 func exit():
 	if is_instance_valid(app_state_node):
-		app_state_node.switch("intro_game")
+		app_state_node.switch("menu")
 	
+
+func _ready() -> void:
+	get_tree().paused = true
+	yield(get_tree().create_timer(4.0,true),"timeout")
+	get_tree().paused = false
+	exit()
